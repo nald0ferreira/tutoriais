ea58964bafe179fd8bf010e8f44ad7220fe2860d
naldo <ednaldocpv@gmail.com>
2023-03-07 18:41:30 -0300

    classePessoa

--- jsDeveloper/05-objetos/aula.js
+++ jsDeveloper/05-objetos/aula.js
@@ -1,19 +1,32 @@
 class Pessoa {
     nome;
     idade;
+    anoDeNascimento;
+
+    constructor(nome, idade){
+        this.nome = nome;
+        this.idade = idade;
+        this.anoDeNascimento = 2022 - idade;
+    }
 
     descrever(){
         return `Meu nome é ${this.nome} e minha idade é ${this.idade}`;
     }
 }
 
-const vitor = new Pessoa();
-vitor.nome = "Vitor J Guerra";
-vitor.idade = 25;
 
-const renam = new Pessoa();
-vitor.nome = "Renam J Paula";
-vitor.idade = 30;
+function compararPessoas(p1, p2){
+    if(p1.idade > p2.idade){
+        console.log(`${p1.nome} é mais velha(o) que ${p2.nome}`);
+    }else if(p2.idade > p1.idade){
+        console.log(`${p2.nome} é mais velha(o) que ${p1.nome}`);
+    }else{
+        console.log(`${p2.nome} e ${p1.nome} tem a mesma idade`);
+    }
+}
 
-vitor.descrever();
+const vitor = new Pessoa('Vitor', 25);
+const renam = new Pessoa('Renan', 30);
+
+compararPessoas(vitor, renam);
 
