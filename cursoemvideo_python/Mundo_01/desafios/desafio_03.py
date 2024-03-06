c1fbfb85811fe217bb8854332d9194e597131172
naldo <ednaldocpv@gmail.com>
2024-03-06 09:11:42 -0300

    inpython

--- cursoemvideo_python/Mundo_01/desafios/desafio_03.py
+++ cursoemvideo_python/Mundo_01/desafios/desafio_03.py
@@ -1,1 +1,7 @@
-#teste no git
+#Crie um programa que leia dois números e mostre a soma entre eles
+
+n1 = 2
+n2 = 4
+s = n1 + n2 
+
+print(f'A soma é:  {s}')
---
+++ cursoemvideo_python/Mundo_01/desafios/desafio_04.py
@@ -0,0 +1,5 @@
+#Faça um programa que leia algo pelo teclado e mostre na tela o seu tipo primitivo e todas as informações possíveis sobre ele.
+
+nome = input('Qual é o seu nome: ')
+tipo = nome.type()
+print(f'Olá {nome} e este é imput é do tipo{tipo}')
