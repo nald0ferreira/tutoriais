096c6f1d71c08682c1e1b8b26a21f8b146bb09af
Ednaldo do Nascimento Ferreira <ednaldocpv@gmail.com>
2024-03-06 23:01:19 -0300

    Update string.py
    update string.py

--- in_python/3 - Tipos de dados/string.py
+++ in_python/3 - Tipos de dados/string.py
@@ -1,1 +1,26 @@
+'''Exemplo de utilização de aspas duplas para que as aspas simples
+possam ser utilizadas'''
+
+texto = "Estou estudando Python no 'Linkedin Learning'"
+
+print(texto)
+
+'''O mesmo ocorre ao contrário, onde primeiro utilizamos aspas duplas 
+e destacamos a palavra com aspas simples'''
+
+texto = 'Estou estudando Python no "Linkedin Learning"'
+
+texto[16:22]
+
+texto.lower()
+texto.upper()
+
+from datetime import datetime
+
+print(f'Data e horário atual: {datetime.now()}')
+
+a = 4
+b = 3
+
+print('A divisão de {} por {} é {:.3f}'.format(a, b, a/b))
 
