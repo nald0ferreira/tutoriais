a705e7f016af426abd6fac3a1327b698ef5ee917
Ednaldo do Nascimento Ferreira <ednaldocpv@gmail.com>
2024-03-11 18:36:27 -0300

    Update booleano.py

--- in_python/3 - Tipos de dados/booleano.py
+++ in_python/3 - Tipos de dados/booleano.py
@@ -1,1 +1,27 @@
+'''
+bool(1)  //True
 
+bool(-1) //True
+
+bool(0)  //False
+
+bool('string') //True
+
+bool('')    //False
+
+bool({})    //False
+
+bool(None)
+'''
+
+minha_lista = [3, 6, None, 80]
+
+if bool(minha_lista) == True:
+  print('Minha lista tem valores')
+
+for item in minha_lista:
+  if item:
+    print(f'{item} é um número válido')
+  else:
+    print(f'{item} é um valor faltante')
+    
