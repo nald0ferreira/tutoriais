5b82bd80b561823dde924d1e7908368b1e906d0f
Ednaldo do Nascimento Ferreira <ednaldocpv@gmail.com>
2024-03-11 22:07:56 -0300

    Update listas.py

--- in_python/4 - Estrutura de dados/listas.py
+++ in_python/4 - Estrutura de dados/listas.py
@@ -9,4 +9,22 @@ print(lista)
 lista[3] =  'sol'
 print(lista)
 
-# Para remover itens da lista podemos 
+'''
+Para remover itens da lista podemos utilizar
+o remove que elimina um elemento específico
+'''
+lista.remove(2)
+
+# A função pop remove o último elemento da lista
+lista.pop()
+print(lista)
+# Agora a lista não possui o número 2 e nem o 60
+
+# Para saber o comprimento da lista utilizamos o len
+comprimento = len(lista)
+print(f'O comprimento da lista é : {comprimento})
+
+# Para fazer a cópia da lista originakl a lista cópia não será alterada
+lista.append('mar')
+print(lista_cópia)
+print(lista)
