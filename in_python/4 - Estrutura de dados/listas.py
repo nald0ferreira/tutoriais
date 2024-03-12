ab1c7c4287f2c48b57c0911211abcb5c4dc40835
Ednaldo do Nascimento Ferreira <ednaldocpv@gmail.com>
2024-03-11 21:42:49 -0300

    Update listas.py

--- in_python/4 - Estrutura de dados/listas.py
+++ in_python/4 - Estrutura de dados/listas.py
@@ -1,1 +1,12 @@
+# Python é uma linguagem de base 0, o primeiro elemento recebe o ídice 0
+print(lista[0])
 
+# A função append adiciona um elemento na lista
+lista.append(60)
+print(lista)
+
+# Para substituir um elemento da lista, faça uma nova atribuição
+lista[3] =  'sol'
+print(lista)
+
+# Para remover itens da lista podemos 
