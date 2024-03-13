3cdfbfa730ba16b993ee80b01824bde59c5dd97d
Ednaldo do Nascimento Ferreira <ednaldocpv@gmail.com>
2024-03-12 22:21:55 -0300

    Update colection.py

--- in_python/4 - Estrutura de dados/colection.py
+++ in_python/4 - Estrutura de dados/colection.py
@@ -1,1 +1,17 @@
+'''
+Colection é um modo nativo do Python que permite criar dicionários
+informando o valor padrão das chaves
+'''
 
+from collections import defaultdict
+
+natureza = defaultdict(list)
+print(natureza)
+
+# A função extend insere varios elementos de uma só vez em uma lista
+natureza['fauna'].extend(['arara', 'capivara', 'jacaré', 'macaco'])
+print(natureza)
+
+# Para localizar um elemento da lista fauna
+print(natureza['fauna'][2])
+# O valor 'jacaré' será impresso na tela
