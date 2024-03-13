483903c4b9a51b3299c4a688834069ae5e7929c7
Ednaldo do Nascimento Ferreira <ednaldocpv@gmail.com>
2024-03-12 22:11:47 -0300

    Update dicionarios.py

--- in_python/4 - Estrutura de dados/dicionarios.py
+++ in_python/4 - Estrutura de dados/dicionarios.py
@@ -7,3 +7,21 @@ material_escritorio = {
 }
 
 # Podemos obter as chaves com a função keys
+print(material_escritorio.keys())
+
+# Para converter as chaves em uma lista utilizamos o list passando o dicionário + keys
+list(material_escritorio.values())
+
+# Também é possível recuperar os valores do dicionário
+print(material_escritorio.values())
+
+# Para adicionar uma nova chave valor 
+material_escritorio['prende'] = 'clip'
+print(material_escritorio)
+
+# É possível criar uma chave que receba uma lista
+material_escritorio['organiza'] = ['agenda', 'kanban']
+
+# Para inserir um novo elemento na lista criada utilizamos append
+material_escritorio['organiza'].append('pasta')
+print(material_escritorio['organiza'])
