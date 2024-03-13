9e00b5c79eaea9cb641d024b4ff48f0c1cd5af48
Ednaldo do Nascimento Ferreira <ednaldocpv@gmail.com>
2024-03-12 21:41:28 -0300

    Create tuplas.py

---
+++ in_python/4 - Estrutura de dados/tuplas.py
@@ -0,0 +1,12 @@
+tupla = (False, True, False, False, True)
+print(tupla[2])
+
+def estacoesAno():
+  return 'primavera', 'verão', 'outono', 'inverno',
+
+tuple
+a, b, c, d, = estacoesAno()
+print(a)
+print(b)
+print(c)
+print(d)
