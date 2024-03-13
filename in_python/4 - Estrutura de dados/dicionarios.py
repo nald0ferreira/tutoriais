622168998eb0921dc654e969ee182e6afdd005f6
Ednaldo do Nascimento Ferreira <ednaldocpv@gmail.com>
2024-03-12 21:46:28 -0300

    Update dicionarios.py

--- in_python/4 - Estrutura de dados/dicionarios.py
+++ in_python/4 - Estrutura de dados/dicionarios.py
@@ -1,1 +1,9 @@
+# Dicionários é uma estrutura de dados do tipo chave valor
 
+material_escritorio = {
+  'escreve':'lápis',
+  'apaga':'borracha',
+  'armazena':'caderno'
+}
+
+# Podemos obter as chaves com a função keys
