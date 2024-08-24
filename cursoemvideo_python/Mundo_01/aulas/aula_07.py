1872277a3bf394bebcfcb0b99bb9ee5cdd91a6bd
Ednaldo do Nascimento Ferreira <ednaldocpv@gmail.com>
2024-08-23 21:10:01 -0300

    Update aula_07.py
    edited

--- cursoemvideo_python/Mundo_01/aulas/aula_07.py
+++ cursoemvideo_python/Mundo_01/aulas/aula_07.py
@@ -1,1 +1,9 @@
+# nome = input('Qual é seu nome')
+# print = print(f'Prazer em te conhecer {nome}!')
 
+n1 = int(input('Um valor: '))
+n2 = int(input('Outro valor'))
+s = n1 + n2
+m = n1 * n2
+d = n1 / n2
+print(f'A soma vale {s}, a multiplicação vale {m} e a divisão vale {d:.3}')
