87759153c6066c244842f52bfd74c5e49689d0a1
Ednaldo do Nascimento Ferreira <ednaldocpv@gmail.com>
2024-08-23 21:26:53 -0300

    Update desafio_05.py

--- cursoemvideo_python/Mundo_01/desafios/desafio_05.py
+++ cursoemvideo_python/Mundo_01/desafios/desafio_05.py
@@ -1,1 +1,5 @@
-
+# Faça um programa que leia um número inteiro e mostre seu sucessor e seu antecessor.
+n1 = int((input('Digite um número ')))
+antecessor = n1 - 1
+sucessor = n1 + 1
+print(f'O número é {n1}, seu antecessor é {antecessor} e seu sucessor é {sucessor}')
